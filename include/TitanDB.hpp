a4ab2269a4b9c1da974eac83a69feaa57ca5f6fd
Abhigyan Tiwari <abhigyanph@gmail.com>
2026-04-16 09:13:13 +0530

    Refactor TitanDB class and update method signatures

--- include/TitanDB.hpp
+++ include/TitanDB.hpp
@@ -1,17 +1,23 @@
+#pragma once
 #include <fstream>
 #include <unordered_map>
+#include <string>
+#include <optional>
 #include "record.hpp"
 
-class TitanDB{
+class TitanDB {
 private:
-    std::fstream file; //f obj declared
-    std::unordered_map<std::string,std::streampos> ind;
+    std::fstream file;
+    std::unordered_map<std::string, std::streampos> ind;
+    bool rec_read_safe(std::fstream& fo, std::streampos offset, Record& r);
 
 public:
-    TitanDB(const std::string & fname);
-    bool rec_read_safe(std::fstream &fo, std::streampos offset, Record &r) ;
-    std::string get(const std::string &key);
-    void put(const std::string &key, const std::string &val);
-    void update(const std::string &key, const std::string &newval);
-    void remove(const std::string &key);
+    TitanDB(const std::string& fname);
+    bool put(const std::string& key, const std::string& val);
+    std::optional<std::string> get(const std::string& key);
+    bool update(const std::string& key, const std::string& val);
+    bool remove(const std::string& key);
+    int size();
+    void printAll();
+    void compact();
 };
